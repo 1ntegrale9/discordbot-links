ef4604011aa0c317c89dc60119c521cfe310f17e
1ntegrale9 <1ntegrale9uation@gmail.com>
2021-08-16 10:15:47 +0900

    [Fix] 環境変数を.envから優先的に読み込むように

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,9 @@
 from os import getenv
+from dotenv import load_dotenv
 from discord.ext import commands
 import discord
 
+load_dotenv(override=True)
 
 bot = commands.Bot(
     command_prefix=commands.when_mentioned_or('$'),
@@ -11,4 +13,3 @@ bot = commands.Bot(
 bot.load_extension('jishaku')
 bot.load_extension('url_extention')
 bot.run(getenv('DISCORD_BOT_TOKEN'))
-
